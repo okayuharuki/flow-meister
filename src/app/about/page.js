83d5b5953171b4e0675562b821db9c9f1d049e3a
okayuharuki <okayuharuki@gmail.com>
2026-02-02 16:17:47 +0900

    輪郭ボタンのコンポーネントを作成

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,6 +2,7 @@ import ButtonSample from "@/components/ui/ButtonSample";
 import CardSample from "@/components/ui/CardSample";
 import MenuSample from "@/components/ui/MenuSample";
 import Button from "@/components/ui/Button";
+import ButtonOutline from "@/components/ui/ButtonOutline";
 
 export default function About() {
   const boxClass = "box";
@@ -10,6 +11,7 @@ export default function About() {
     <>
       <div className={boxClass}>
         <Button href="#" text="導入事例をすべて見る" />
+        <ButtonOutline href="#" text="導入事例をすべて見る" />
 
         <p style={{ color: "#333", backgroundColor: "pink" }}>
           段落が入ります。段落が入ります。
