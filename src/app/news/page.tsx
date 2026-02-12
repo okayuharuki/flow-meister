e86226086673d97eed1942b10ed1bcf7f44847cc
okayuharuki <okayuharuki@gmail.com>
2026-02-12 10:55:13 +0900

    2カラム用のレイアウトを作成

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,7 +1,13 @@
 import PageFirstView from "@/components/layout/PageFirstView";
+import TwoColumn from "@/components/layout/TwoColumn";
 
 export default function News() {
   return (
-    <PageFirstView subText="News" mainText="お知らせ" textAlign="isCenter" />
+    <>
+      <PageFirstView subText="News" mainText="お知らせ" textAlign="isCenter" />
+      <TwoColumn>
+        <p>お知らせページです。</p>
+      </TwoColumn>
+    </>
   );
 }
