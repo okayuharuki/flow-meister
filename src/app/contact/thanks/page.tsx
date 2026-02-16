397c725b130245b320c8872eb87c9ad23c14fb53
okayuharuki <okayuharuki@gmail.com>
2026-02-16 15:56:49 +0900

    完了ページの作成

--- src/app/contact/thanks/page.tsx
+++ src/app/contact/thanks/page.tsx
@@ -1,6 +1,39 @@
-export default function Thanks() {
-    return(
-        <p>お問い合わせ完了用のページです。</p>
-    )
-}
+import PageFirstView from "@/components/layout/PageFirstView";
+import OneColumn from "@/components/layout/OneColumn";
+import Image from "next/image";
+import ButtonOutline from "@/components/ui/ButtonOutline";
+import styles from "./Thanks.module.css";
 
+export default function Thanks() {
+  return (
+    <>
+      <PageFirstView
+        subText="Contact"
+        mainText="お問い合わせ"
+        textAlign="isCenter"
+      />
+      <OneColumn>
+        <div className={styles.pageThanks}>
+          <p className={styles.lead}>Thank you!</p>
+          <p className={styles.text}>
+            お問い合わせありがとうございました。
+            <br />
+            2～3営業日以内に、担当よりメールにて返信いたしますので、しばらくお待ちください。
+          </p>
+          <Image
+            src="/thanks.png"
+            alt="Thank you!"
+            width="320"
+            height="160"
+            loading="lazy"
+            decoding="async"
+            className={styles.image}
+          />
+          <div className={styles.button}>
+            <ButtonOutline href="/" text="ホームへ戻る" />
+          </div>
+        </div>
+      </OneColumn>
+    </>
+  );
+}
