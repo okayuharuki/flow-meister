c3a20c0e9889672bb3570ae8c804cc664eed72a2
okayuharuki <okayuharuki@gmail.com>
2026-03-02 13:48:48 +0900

    SSGの設定をコメントアウト

--- src/app/news/[id]/page.tsx
+++ src/app/news/[id]/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = "force-static";
+// export const dynamic = "force-static";
 
 import PageFirstView from "@/components/layout/PageFirstView";
 import TwoColumn from "@/components/layout/TwoColumn";
